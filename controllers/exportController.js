9682aedc8778a360ce7b1150d1180170abed530f
haeun161 <80445078+haeun161@users.noreply.github.com>
2023-12-06 18:14:06 +0900

    fix: jwt.verify(token, process.env.jwtsecret);

--- controllers/exportController.js
+++ controllers/exportController.js
@@ -14,7 +14,7 @@ function sleep(ms) {
 exports.postSummary = async function (req, res) {
   const token = req.cookies.x_auth;
   if (token) {
-    const decodedToken = jwt.verify(token, secret.jwtsecret); // 토큰 검증, 복호화
+    const decodedToken = jwt.verify(token, process.env.jwtsecret); // 토큰 검증, 복호화
     const user_id = decodedToken.user_id; // user_id를 추출
     const { date1 } = req.body;
 
