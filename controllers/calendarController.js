96d73e6423306ca01ad83496b83e347be9c905c3
root <root@instance-2.c.open-398711.internal>
2023-12-06 08:47:32 +0000

    fix: secret.js to deployment.yaml 변수수

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -190,8 +190,9 @@ exports.postFile = async function (req, res) {
 exports.postMindDiary = async function (req, res) {
   const token = req.cookies.x_auth;
   if (token) {
-      const decodedToken = jwt.verify(token, secret.jwtsecret); // 토큰 검증, 복호화
+      const decodedToken = jwt.verify(token,process.env.jwtsecret); // 토큰 검증, 복호화
       const user_id = decodedToken.user_id; // user_id를 추출
+      console.log(req.body);
       const date = req.query.selectedYear + req.query.selectedMonth + req.query.selectedDate; //쿼리스트링에서 날짜 추출
       //const date = '2023-09-20'
       console.log(req.query);
